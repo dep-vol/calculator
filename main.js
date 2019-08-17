cf08a136e901103f7138529230531aa232d44051
tecto <dep-vol@mail.ru>
2019-08-17 16:40:05 +0300

    Final version

--- main.js
+++ main.js
@@ -22,6 +22,9 @@ function Calculator(){
     this._ctan = document.getElementById('ctan');
     this._cube = document.getElementById('cube');
     this._percent = document.getElementById('percent');
+    this._pi = document.getElementById('pi');
+    this._tenPow = document.getElementById('tenPow');
+    this._radical = document.getElementById('radical');
     this._resBtn = document.getElementById('resBtn');
     //................Properties.................
     this._firstNum = '';
@@ -47,6 +50,7 @@ function Calculator(){
     //Add listener to numbers
     
     let numListen = (e) => {
+        this._input.focus();
         this._input.value += e.target.innerText;
     }; 
     this.numbers.forEach(element => {
@@ -60,9 +64,11 @@ function Calculator(){
         if(negPos == '-') {
             let newNum = this._input.value.slice(1);
             this._input.value = newNum;
+            this._input.focus();
         }
         else {
             this._input.value = '-'+ this._input.value;
+            this._input.focus();
         }
          
         
@@ -76,6 +82,7 @@ function Calculator(){
         this._firstNum = '';
         this._secNum = '';
         this._result = '';
+        this._input.focus();
     };
     this._clearBtn.addEventListener('click', this._clearInput.bind(this));
 
@@ -83,6 +90,7 @@ function Calculator(){
     //Backing sym method
     this._backspaceSym = function () {
         this._input.value = this._input.value.slice(0, this._input.value.length - 1);
+        this._input.focus();
     }
 
     this._backspaceBtn.addEventListener('click', this._backspaceSym.bind(this));
@@ -94,7 +102,7 @@ function Calculator(){
         if(this._input.value.indexOf('.') == -1 ) {
             this._input.value = this._input.value + '.';
         }
-        
+        this._input.focus();        
     }
 
     this._dot.addEventListener('click', this._pasteDot.bind(this));
@@ -104,12 +112,12 @@ function Calculator(){
     this._chooseOperation = function (op) {
         this._operation = op;
         this._firstNum = this._input.value;
+        this._input.focus();
 
         //Ignore '=' if clicked 'one-operation' btn such as sin, cos....
 
         if ((op == 'sqr') || (op == 'sin') || (op == 'cos') || (op == 'tan') ||
-            (op == 'ctan') || (op == 'cube')) 
-        {
+            (op == 'ctan') || (op == 'cube') || (op == 'pi') || (op == 'tenPow')) {
             this._makeResult();
         }
         else {
@@ -119,8 +127,9 @@ function Calculator(){
 
     }
 
-    
+
     this._makeResult = function () {
+        this._input.focus();
         switch (this._operation) {
             case 'plus':
                 if (!this._result) {
@@ -176,38 +185,56 @@ function Calculator(){
             case 'pow':
                 if (!this._result) {
                     this._secNum = this._input.value;
-                    this._result = Math.pow(+this._firstNum,+this._secNum);
+                    this._result = Math.pow(+this._firstNum, +this._secNum);
                     this._input.value = this._result;
                 }
                 else {
-                    this._result = Math.pow(this._result,+this._secNum);
+                    this._result = Math.pow(this._result, +this._secNum);
                     this._input.value = this._result;
 
                 }
                 break;
-                case 'sin':
+            case 'sin':
                 this._input.value = Math.sin(this._firstNum);
                 break;
-                case 'cos':
+            case 'cos':
                 this._input.value = Math.cos(this._firstNum);
                 break;
-                case 'tan':
+            case 'tan':
                 this._input.value = Math.tan(this._firstNum);
                 break;
-                case 'ctan':
-                this._input.value = 1/(Math.tan(this._firstNum));
+            case 'ctan':
+                this._input.value = 1 / (Math.tan(this._firstNum));
                 break;
-                case 'cube':
-                this._input.value = Math.pow(this._firstNum,3);
+            case 'cube':
+                this._input.value = Math.pow(this._firstNum, 3);
                 break;
-                case 'percent':
+            case 'percent':
                 if (!this._result) {
                     this._secNum = this._input.value;
-                    this._result = ((+this._firstNum)/100)*+this._secNum;
+                    this._result = ((+this._firstNum) / 100) * +this._secNum;
                     this._input.value = this._result;
                 }
                 else {
-                    this._result = ((+this._result)/100)/this._secNum;
+                    this._result = ((+this._result) / 100) / this._secNum;
+                    this._input.value = this._result;
+
+                }
+                break;
+            case 'pi':
+                this._input.value = Math.PI;
+                break;
+            case 'tenPow':
+                this._input.value = Math.pow(10, this._firstNum);
+                break;
+            case 'radical':
+                if (!this._result) {
+                    this._secNum = this._input.value;
+                    this._result = Math.pow(+this._firstNum, (1 / +this._secNum));
+                    this._input.value = this._result;
+                }
+                else {
+                    this._result = Math.pow(this._result, (1 / +this._secNum));
                     this._input.value = this._result;
 
                 }
@@ -229,7 +256,49 @@ function Calculator(){
     this._ctan.addEventListener('click', this._chooseOperation.bind(this,'ctan'));
     this._cube.addEventListener('click', this._chooseOperation.bind(this,'cube'));
     this._percent.addEventListener('click', this._chooseOperation.bind(this,'percent'));
+    this._pi.addEventListener('click', this._chooseOperation.bind(this,'pi'));
+    this._tenPow.addEventListener('click', this._chooseOperation.bind(this,'tenPow'));
+    this._radical.addEventListener('click', this._chooseOperation.bind(this,'radical'))
     this._resBtn.addEventListener('click', this._makeResult.bind(this));
+
+    //Checking input
+    this._checkKeyInput = function (e) {
+        if (e.charCode && ((e.charCode < 48 || e.charCode > 57)) && e.charCode != 46) {
+            e.preventDefault();
+        }
+
+        //Plus key
+
+        if (e.charCode == 43) {
+            this._chooseOperation.bind(this,'plus')();
+        }
+
+        //Minus key
+
+        if (e.charCode == 45) {
+            this._chooseOperation.bind(this,'minus')();
+        }
+
+        //Multiply key
+
+        if (e.charCode == 42) {
+            this._chooseOperation.bind(this,'multiply')();
+        }
+        
+        //Divide key
+
+        if (e.charCode == 47) {
+            this._chooseOperation.bind(this,'divide')();
+        }
+
+        //Divide key
+
+        if ((e.charCode == 61) || (e.charCode == 13)) {
+            this._makeResult.bind(this)();
+        }
+    }
+
+    this._input.addEventListener('keypress', this._checkKeyInput.bind(this));
 
 } 
 
